1c3b8d002e81b196d785f7713f6be63098d76ae8
a5q2531 <sugata@vip.qq.com>
2019-08-20 18:57:50 +0800

    这也只是个测试

--- src/A.java
+++ src/A.java
@@ -4,5 +4,10 @@
  * @date 2019/8/20
  */
 public class A {
+    public static void main(String[] args) {
+        System.out.println("我");
+        System.out.println("我");
+        System.out.println("我");
+    }
 }
 
