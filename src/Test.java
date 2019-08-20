bbf735bc73ab8808cc28b5d6d43e3977aafce0e1
a5q2531 <sugata@vip.qq.com>
2019-08-20 17:52:20 +0800

    这只是个测试

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @author zhengtx
+ * @description dfbz
+ * @date 2019/8/20
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+
+}
+
