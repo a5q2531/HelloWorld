66b9cacb891a66747226c67da9682be3b6bd674d
a5q2531 <sugata@vip.qq.com>
2019-08-20 18:03:27 +0800

    这也只是个测试

---
+++ src/NewTest1.java
@@ -0,0 +1,11 @@
+/**
+ * @author zhengtx
+ * @description dfbz
+ * @date 2019/8/20
+ */
+public class NewTest1 {
+    public static void main(String[] args) {
+        System.out.println("拉拉了");
+    }
+}
+
